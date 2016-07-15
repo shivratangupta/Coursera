cada585a7e6a3c1916d9a7185e6e0251f863d912
shivratangupta <shivratangupta18@gmail.com>
2016-07-15 16:34:46 +0530

    Module 4 Solution

---
+++ HTML-CSS-JS/Module-4-Solution/play.js
@@ -0,0 +1,6 @@
+//not part of assignment, just for fun 
+
+function play() {
+	document.getElementById("content").innerHTML="One Thing About You <br> You're Here to Grade My Work! <br> And I Thank You for That!"
+	document.getE
+}
